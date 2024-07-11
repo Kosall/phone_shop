6fdf7ba27095d55a7ebd0f0dcdf1b1558189f556
Kosall Nim <kosall123nim@gmail.com>
2024-07-11 16:39:12 +0700

    extend JpaSpecificationExecutor in productImportHistory

--- src/main/java/org/kosal/phoneshop/kosal1_phoneshop/repository/ProductImportHistoryRepository.java
+++ src/main/java/org/kosal/phoneshop/kosal1_phoneshop/repository/ProductImportHistoryRepository.java
@@ -2,9 +2,10 @@ package org.kosal.phoneshop.kosal1_phoneshop.repository;
 
 import org.kosal.phoneshop.kosal1_phoneshop.entities.ProductImportHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface ProductImportHistoryRepository extends JpaRepository<ProductImportHistory,Long>{
+public interface ProductImportHistoryRepository extends JpaRepository<ProductImportHistory,Long>,JpaSpecificationExecutor<ProductImportHistory>{
 	
 	
 
