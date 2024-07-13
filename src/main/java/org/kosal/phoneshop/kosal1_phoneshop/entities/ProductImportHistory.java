e51f085fd8aaed24162508054cd0d06bfc57283f
Kosall Nim <kosall123nim@gmail.com>
2024-07-13 13:08:12 +0700

    Ignore

--- src/main/java/org/kosal/phoneshop/kosal1_phoneshop/entities/ProductImportHistory.java
+++ src/main/java/org/kosal/phoneshop/kosal1_phoneshop/entities/ProductImportHistory.java
@@ -12,10 +12,16 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name="import_product_histories")
 public class ProductImportHistory {
 	@Id
