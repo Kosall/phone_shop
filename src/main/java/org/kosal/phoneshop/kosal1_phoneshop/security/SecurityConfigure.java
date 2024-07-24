7f5a0238279e73ab8b1f0aeb98141f637f9eaf8c
Kosall Nim <kosall123nim@gmail.com>
2024-07-24 15:43:19 +0700

    add some features

--- src/main/java/org/kosal/phoneshop/kosal1_phoneshop/security/SecurityConfigure.java
+++ src/main/java/org/kosal/phoneshop/kosal1_phoneshop/security/SecurityConfigure.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,48 +23,53 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableGlobalMethodSecurity(
-		prePostEnabled = true,
-		securedEnabled = true,
-		jsr250Enabled = true
-		
-		)
-public class SecurityConfigure extends WebSecurityConfigurerAdapter{
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true
+
+)
+public class SecurityConfigure extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+	@Autowired
+	private UserDetailsService detailsService;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-		.addFilter(new JwtFilter(authenticationManager()))
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and()
-		.addFilterAfter(new TokeVerifyFilter(),JwtFilter.class)
-		.authorizeHttpRequests()
-		.antMatchers("/","index.html","cs/**","js/**").permitAll()
-		//.antMatchers("/models").hasRole(RoleEnum.SALE.name())
-		//.antMatchers("/brands").hasRole("SALE")
-		//.antMatchers(HttpMethod.POST,"/brand").hasAnyAuthority("brand:write")
-		//.antMatchers(HttpMethod.POST,"/brands").hasAnyAuthority(BRAND_WRITE.getDescription())
-		//.antMatchers(HttpMethod.GET,"/brands").hasAnyAuthority(BRAND_READ.getDescription())
-		.anyRequest()
-		.authenticated();
-		
-	}
-	@Bean
-	@Override
-	protected UserDetailsService userDetailsService() {
-		UserDetails user = User.builder()
-		.username("nita")
-		.password( passwordEncoder.encode("nita01"))
-		.authorities(RoleEnum.SALE.grantedAuthorities())
-		.build();
-		UserDetails user2 = User.builder()
-		.username("nary")
-		.password(passwordEncoder.encode("nary01"))
-		.authorities(RoleEnum.ADMIN.grantedAuthorities())
-		.build();
-		UserDetailsService detailsService=new InMemoryUserDetailsManager(user,user2);
-		return detailsService;
+		http.csrf().disable().addFilter(new JwtFilter(authenticationManager())).sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.addFilterAfter(new TokeVerifyFilter(), JwtFilter.class).authorizeHttpRequests()
+				.antMatchers("/", "index.html", "cs/**", "js/**").permitAll()
+				// .antMatchers("/models").hasRole(RoleEnum.SALE.name())
+				// .antMatchers("/brands").hasRole("SALE")
+				// .antMatchers(HttpMethod.POST,"/brand").hasAnyAuthority("brand:write")
+				// .antMatchers(HttpMethod.POST,"/brands").hasAnyAuthority(BRAND_WRITE.getDescription())
+				// .antMatchers(HttpMethod.GET,"/brands").hasAnyAuthority(BRAND_READ.getDescription())
+				.anyRequest().authenticated();
+
 	}
 
+	/*
+	 * @Bean
+	 * 
+	 * @Override protected UserDetailsService userDetailsService() { UserDetails
+	 * user = User.builder() .username("nita") .password(
+	 * passwordEncoder.encode("nita01"))
+	 * .authorities(RoleEnum.SALE.grantedAuthorities()) .build(); UserDetails user2
+	 * = User.builder() .username("nary")
+	 * .password(passwordEncoder.encode("nary01"))
+	 * .authorities(RoleEnum.ADMIN.grantedAuthorities()) .build();
+	 * UserDetailsService detailsService=new InMemoryUserDetailsManager(user,user2);
+	 * return detailsService; }
+	 */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(getAuthenticationProvider());
+	}
+	
+	@Bean
+	public AuthenticationProvider getAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(detailsService);
+		provider.setPasswordEncoder(passwordEncoder);
+		return provider;
+	}
 }
