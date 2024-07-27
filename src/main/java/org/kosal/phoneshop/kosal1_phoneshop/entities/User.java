b95a5ea7b79bbf78a9c54003968c466607e8a7db
Kosall Nim <kosall123nim@gmail.com>
2024-07-27 19:33:28 +0700

    add roles entity with ManyToMany annotation

--- src/main/java/org/kosal/phoneshop/kosal1_phoneshop/entities/User.java
+++ src/main/java/org/kosal/phoneshop/kosal1_phoneshop/entities/User.java
@@ -1,14 +1,14 @@
 package org.kosal.phoneshop.kosal1_phoneshop.entities;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-
-import org.kosal.phoneshop.kosal1_phoneshop.security.RoleEnum;
 
 import lombok.Data;
 
@@ -19,15 +19,15 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String firsName;
+	private String firstName;
 	private String lastName;
 	private String username;
 	private String password;
-	@Enumerated(EnumType.STRING)
-	private RoleEnum role;
 	private boolean accountNonExpired;
 	private boolean accountNonLocked;
 	private boolean credentialsNonExpired;
 	private boolean enabled;
+	@ManyToMany(fetch = FetchType.EAGER)
+	private Set<Role> roles;
 
 }
