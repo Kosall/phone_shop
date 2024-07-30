223549cfc06eb974f06f62fd2c8623ee8b4d5fbf
Kosall Nim <kosall123nim@gmail.com>
2024-07-30 16:19:33 +0700

    create Example.java

---
+++ src/main/java/org/kosal/phoneshop/kosal1_phoneshop/security/JWT/Example.java
@@ -0,0 +1,16 @@
+package org.kosal.phoneshop.kosal1_phoneshop.security.JWT;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+
+public class Example {
+
+	public static void main(String[] args) {
+		String x="I ";
+		String y="love ";
+		String z="you";
+		log.info(x+y+z);
+	}
+
+}
