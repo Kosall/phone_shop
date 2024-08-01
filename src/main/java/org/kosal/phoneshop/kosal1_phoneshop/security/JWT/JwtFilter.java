f465e7af2bd2e83fb21035f45b7e035a8e50307f
Kosall Nim <kosall123nim@gmail.com>
2024-08-01 13:12:19 +0700

    change value of keygend in successfulAuthentication()

--- src/main/java/org/kosal/phoneshop/kosal1_phoneshop/security/JWT/JwtFilter.java
+++ src/main/java/org/kosal/phoneshop/kosal1_phoneshop/security/JWT/JwtFilter.java
@@ -20,37 +20,38 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+
 @RequiredArgsConstructor
 public class JwtFilter extends UsernamePasswordAuthenticationFilter {
-	
+
 	private final AuthenticationManager authenticationManager;
+
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
 			throws AuthenticationException {
-		ObjectMapper mapper=new ObjectMapper();
+		ObjectMapper mapper = new ObjectMapper();
 		try {
 			LoginRequest loginRequest = mapper.readValue(request.getInputStream(), LoginRequest.class);
-			Authentication authentication =new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword());
-			return authenticationManager.authenticate(authentication);
-		}catch (Exception e) {
+			Authentication authentication = new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),loginRequest.getPassword());
+			Authentication authenticate = authenticationManager.authenticate(authentication);
+			return authenticate;
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			throw new RuntimeException(e);
 		}
-		//return super.attemptAuthentication(request, response);
+		// return super.attemptAuthentication(request, response);
 	}
-@Override
-protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-		Authentication authResult) throws IOException, ServletException {
-			String keygend="kosal123kosal123kosal123kosal123";
-	String token = Jwts.builder()
-			.setSubject(authResult.getName())
-			.claim("authorities", authResult.getAuthorities())
-			.setIssuedAt(new Date())
-			.setExpiration(java.sql.Date.valueOf(LocalDate.now().plusDays(7)))
-			.setIssuer("Phoneshop.com")
-			.signWith(Keys.hmacShaKeyFor(keygend.getBytes()))
-			.compact();
-	response.setHeader("Authorization","Bearer "+ token);
-}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+		String keygend="l988877736622991002992118828229888777";
+		String token = Jwts.builder().setSubject(authResult.getName()).setIssuedAt(new Date())
+				.claim("authorities", authResult.getAuthorities())
+				.setExpiration(java.sql.Date.valueOf(LocalDate.now().plusDays(7)))
+				.setIssuer("phoneshop.com")
+				.signWith(Keys.hmacShaKeyFor(keygend.getBytes())).compact();
+		response.setHeader("Authorization", "Bearer " + token);
+	}
 
 }
