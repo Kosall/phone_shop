24031ad406d6ab29dadd69da078c6d915445a1b3
Kosall Nim <kosall123nim@gmail.com>
2024-08-01 13:24:06 +0700

    there are no changes for this.

--- src/main/java/org/kosal/phoneshop/kosal1_phoneshop/security/UserDetailsServiceImpl.java
+++ src/main/java/org/kosal/phoneshop/kosal1_phoneshop/security/UserDetailsServiceImpl.java
@@ -1,12 +1,8 @@
 package org.kosal.phoneshop.kosal1_phoneshop.security;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
