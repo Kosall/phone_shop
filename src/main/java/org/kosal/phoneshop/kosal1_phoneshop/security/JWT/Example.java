de1acce45e911f07474dc83ef5b970722cd1be40
Kosall Nim <kosall123nim@gmail.com>
2024-08-05 21:14:52 +0700

    g

--- src/main/java/org/kosal/phoneshop/kosal1_phoneshop/security/JWT/Example.java
+++ src/main/java/org/kosal/phoneshop/kosal1_phoneshop/security/JWT/Example.java
@@ -7,10 +7,32 @@ import lombok.extern.slf4j.Slf4j;
 public class Example {
 
 	public static void main(String[] args) {
-		String x="I ";
-		String y="love ";
-		String z="you";
-		log.info(x+y+z);
+
+		log.info(getYou() + getAre() + getMy()+getLove().toUpperCase());
+		
+		
+		
 	}
+
+	public static String getAre() {
+
+		return "are ";
+	}
+
+	public static String getLove() {
+
+		return "love.";
+	}
+
+	public static String getYou() {
+
+		return "You ";
+	}
+	public static String getMy() {
+
+		return "my ";
+	}
+
+	
 
 }
